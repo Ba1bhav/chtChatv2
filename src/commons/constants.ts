7eae3e8e85b5faa11913dc149b10d86bf3a2c043
Ba1bhav <baibhav.kumar@chicmic.co.in>
2023-04-07 19:14:05 +0530

    new UI

--- src/commons/constants.ts
+++ src/commons/constants.ts
@@ -4,5 +4,6 @@ export const urls={
   phoneUrl:'/users/',
   metaMaskUrl:'/dusers/',
   storage:'https://firebasestorage.googleapis.com/v0/b/chitchatv2-f816e.appspot.com/o/',
-  defaultProfile:'../assets/images/anonymous-avatar-icon-19.webp'
+  defaultProfile:'../assets/images/anonymous-avatar-icon-19.webp',
+  chatsBackground:'../assets/images/chatBackground.svg'
 }
--- src/modules/shared/components/search-user/search-user.component.ts
+++ src/modules/shared/components/search-user/search-user.component.ts
@@ -1,10 +1,12 @@
 import { Component} from '@angular/core';
+import { urls } from 'src/commons/constants';
 @Component({
   selector: 'app-search-user',
   templateUrl: './search-user.component.html',
   styleUrls: ['./search-user.component.scss']
 })
 export class SearchUserComponent {
+  urls=urls
 logOut(){
   localStorage.clear()
 }
