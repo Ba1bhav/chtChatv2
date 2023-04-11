90d34f055498e71ff3f53d25917bf243180f22b8
Ba1bhav <baibhav.kumar@chicmic.co.in>
2023-04-11 19:05:14 +0530

    Chats Integrated

--- src/modules/shared/components/chat/chat.component.ts
+++ src/modules/shared/components/chat/chat.component.ts
@@ -1,35 +1,101 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit,AfterViewChecked,ChangeDetectorRef, AfterViewInit} from '@angular/core';
 import { urls } from 'src/commons/constants';
 import { FirebaseService } from 'src/services/shared/firebase.service';
-import{doc,getDoc,setDoc,updateDoc,collection, arrayUnion,FieldValue} from'firebase/firestore'
+import{doc,arrayUnion,updateDoc, onSnapshot} from'firebase/firestore'
+import { HttpRequestsService } from 'src/services/shared/http-requests.service';
+import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit{
+export class ChatComponent implements OnInit,AfterViewChecked,AfterViewInit{
 userData: any;
 dataBase:any;
 dataBaseReffrence:any;
 chatsReff:any;
-constructor(private fireBaseService:FirebaseService){
+messages:any=[]
+senderId=localStorage.getItem('uid');
+unsubscribeListener:any;
+attachmentFile:any;
+attachmentFileUrl:any;
+imageExtensions=['jpg','jpeg','gif','png','webp']
+constructor(private httpRequests:HttpRequestsService,private fireBaseService:FirebaseService,private changeDetector:ChangeDetectorRef){
   this.dataBase=fireBaseService.getDb();
-  this.dataBaseReffrence=doc(this.dataBase,'chats','11900191');
-  this.chatsReff=collection(this.dataBase, "chats");
+  this.dataBaseReffrence=doc(this.dataBase,'chats','NvuhjlRARx7AF2Fo6TGV');
+  this.chatsReff=doc(this.dataBase, "chats",'NvuhjlRARx7AF2Fo6TGV');
+
 }
+
 ngOnInit() {
-    updateDoc(doc(this.chatsReff, "pU3Q54x4yvX8pJa8yZnM5iMjN5D2"), {
-      rooms:arrayUnion({roomName: "Room 2", roomMembers:['11900192'], roomMessages:[{owner:'11900191',message:'hello'}],
-      roomLogo:"temp.jpg"})
-
-
-    }).then((response)=>{
-      console.log(response);
-      getDoc(this.dataBaseReffrence).then((response:any)=>{console.log(response.data())}).catch((error)=>console.log(error))
-    })
-
+  this.unsubscribeListener = onSnapshot(doc(this.dataBase, "chats",'NvuhjlRARx7AF2Fo6TGV'), (doc) => {
+    this.messages=doc?.data()?.['messages']})
 }
+ngAfterViewInit(){
+  this.changeDetector.detectChanges()
+}
+ngAfterViewChecked() {
+  this.changeDetector.detectChanges()
+}
+
 errorImageHandler(imageEvent:any) {
   imageEvent.target.src=urls.defaultProfile;
   }
+sendMessage(message:any,type:number){
+  if(type=1){
+    const messageData=message;
+    updateDoc(this.chatsReff, {messages:arrayUnion({message:messageData,senderId:this.senderId,type:type,date:String(new Date())})}).then((response:any)=>{console.log('Message Send Successfully');
+  })
+  }
+  else{
+  const messageData=message.value;
+  updateDoc(this.chatsReff, {messages:arrayUnion({message:messageData,senderId:this.senderId,type:type,date:String(new Date())})}).then((response:any)=>{console.log('Message Send Successfully');
+  })
+  message.value='';
+}
+}
+fileAttach(fileEvent:any){
+this.attachmentFile=fileEvent?.srcElement?.files[0];
+this.attachmentFile.isImage=this.imageExtensions.includes(this.attachmentFile.type.split('/')[1].toLowerCase())
+let reader = new FileReader();
+reader.readAsDataURL(fileEvent?.srcElement?.files[0]);
+reader.onload = (_event) => {
+  this.attachmentFileUrl = reader.result;
+}
+}
+
+attchmentUpload(){
+    const app=this.fireBaseService.app()
+    const storage=getStorage(app)
+    const imageref=ref(storage,'/'+this.attachmentFile?.name)
+    const uploadTask = uploadBytesResumable(imageref, this.attachmentFile);
+    uploadTask.on('state_changed',
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case 'paused':
+          console.log('Upload is paused');
+          break;
+        case 'running':
+          console.log('Upload is running');
+          break;
+      }
+    },
+    (error) => {
+      console.log(error)
+      }
+    ,
+    () => {
+      this.httpRequests.getDownloadLink(this.attachmentFile?.name).subscribe((response:any)=>{
+        const downloadUrl=urls.storage+this.attachmentFile?.name+'?alt=media&token='+response?.downloadTokens;
+        this.attachmentFile=null;
+        this.sendMessage(downloadUrl,1)
+      })
+    })
+}
+  deleteMessage(index:any){
+    this.messages.splice(index,1)
+  }
+
 }
--- src/modules/shared/components/search-user/search-user.component.ts
+++ src/modules/shared/components/search-user/search-user.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import { urls } from 'src/commons/constants';
 import { Router } from '@angular/router';
 import { FirebaseService } from 'src/services/shared/firebase.service';
-import { getDoc,collection,doc } from 'firebase/firestore';
+import { getDoc,collection,doc, where, query, getDocs, addDoc, setDoc } from 'firebase/firestore';
 @Component({
   selector: 'app-search-user',
   templateUrl: './search-user.component.html',
@@ -14,27 +14,94 @@ export class SearchUserComponent implements OnInit{
   dataBaseReffrence:any;
   chatsReff:any;
   userData: any;
-  chatsList:any;
+  chatsList:any=[];
+  usersChatlistsReff: any;
+  searchInput:any;
+  searchInputDebounce:any;
+  searchResult:any=[];
+  userschatReff:any;
 constructor(private router:Router,private fireBaseService:FirebaseService){
   this.dataBase=fireBaseService.getDb();
   this.dataBaseReffrence=doc(this.dataBase,'chats',localStorage.getItem('uid')||'pU3Q54x4yvX8pJa8yZnM5iMjN5D2');
   this.chatsReff=collection(this.dataBase, "chats");
+  this.usersChatlistsReff=collection(this.dataBase, 'usersChatlists',localStorage.getItem('uid')??'','chats');
+
 }
 ngOnInit() {
-
-  getDoc(this.dataBaseReffrence).then((response:any)=>{
-    console.log(response.data(),'chats');
-    this.chatsList=response.data()?.rooms;
-  }).catch((error)=>console.log(error))
-
-
+  getDocs(this.usersChatlistsReff).then((response:any)=>
+  {response.forEach((result:any)=>{
+    console.log(result.id,'->',result.data());
+    this.chatsList.push(result.data())
+  })})
 }
-
-  errorImageHandler(imageEvent:any) {
+errorImageHandler(imageEvent:any) {
     imageEvent.target.src=urls.defaultProfile;
     }
 logOut(){
   localStorage.clear()
   this.router.navigate(['/login'])
 }
+searchUser(inputEvent:any){
+console.log(inputEvent?.value);
+clearTimeout(this.searchInputDebounce)
+this.searchInputDebounce=setTimeout(()=>{
+  console.log('Searching ... ');
+  this.searchQuery(Array.from(inputEvent.value))
+},1000)
+}
+searchQuery(searchArray:any){
+  console.log('response');
+if(searchArray.length>0){
+  const search=query(collection(this.dataBase, 'usersChatlists'), where('userName','array-contains-any',searchArray));
+  this.searchResult=[]
+  getDocs(search).then((response:any)=>response.forEach((doc:any) => {
+    this.searchResult.push({id:doc.id,name:String(doc.data().userName).replaceAll(',','')})
+    console.log(doc.id, ' => ', doc.data());
+}))
+}
+else{
+  this.searchResult=null;
+}
+  }
+
+createChat(uid:any){
+    const senderId=localStorage.getItem('uid')??'';
+    this.userschatReff=collection(this.dataBase,'usersChatlists',senderId,'chats');
+    const idArray:any=[]
+    getDocs(this.userschatReff).then((response:any)=>
+    {response.forEach((result:any)=>{
+      console.log(result.id);
+      idArray.push(result.id);
+
+    })
+    console.log(idArray,uid);
+
+    if(idArray.includes(uid)){
+      console.log('Chat All ready Exists')
+    }
+    else{
+      let chatId:any;
+      console.log('creating new chat with ',uid);
+      addDoc(this.chatsReff, {messages:[]}).then((response:any)=>{
+
+        chatId=response.id
+        const recieverId=uid;
+        const senderData={id:chatId,reciever:recieverId};
+        const recieverData={id:chatId,reciever:senderId};
+        const sender=collection(this.dataBase,'usersChatlists',senderId,'chats');
+        const reciever=collection(this.dataBase,'usersChatlists',recieverId,'chats');
+        // console.log(data,senderId,recieverId);
+
+        setDoc(doc(sender,recieverId),senderData).then((response:any)=>console.log(response||'Success-Sender')).catch(()=>console.log('Error'))
+        setDoc(doc(reciever,senderId),recieverData).then((response:any)=>console.log(response||'Success-Reciever')).catch(()=>console.log('Error'))
+
+
+      })
+
+      //{id:chatId,name=userName}
+      //create message id
+      //add reffrence object to usersChatlists/userId/chats to both the users
+    }
+    })
+  }
 }
